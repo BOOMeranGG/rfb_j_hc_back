5ff6e49ad19feac12adec39b03f6dade8f09c0b0
Prokhorov Grigorii <41330437+vvn13@users.noreply.github.com>
2019-09-15 13:21:27 +0300

    Update repository

--- src/main/java/com/orange/freelance_rfb/repository/AccountRepository.java
+++ src/main/java/com/orange/freelance_rfb/repository/AccountRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccountRepository
-    extends JpaRepository<Account, String> {
-    List<Account> findByLogin(String login);
+    extends JpaRepository<Account, Long> {
+    //List<Account> findByLogin(String login);
+    Account findByLogin(String login);
 }
--- src/main/java/com/orange/freelance_rfb/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.orange.freelance_rfb.repository;
-
-import com.orange.freelance_rfb.model.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository
-    extends JpaRepository<Order, Long> {
-}
